a5cd22fc91e6e82266219fce37e11b6c06e7139b
JuanMa0228 <jgaviria@intersepcha.com>
2024-09-28 23:34:58 -0500

    front de actividad 4

--- Calculadora/views.py
+++ Calculadora/views.py
@@ -1,24 +1,30 @@
+import json
+import math
 from django.views.generic import View
 from django.shortcuts import render
+from django.http import JsonResponse
+
 
 class CalculadoraView(View):
     def get(self, request, *args, **kwargs):
         return render(request, "index.html")
     
     def post(self, request, *args, **kwargs):
-        tipo = request.POST.get("tipo_operacion", None)
-        valor_1 = request.POST.get("valor_1", None)
-        valor_2 = request.POST.get("valor_2", None)
+        data = json.loads(request.body)
+        tipo = data.get("tipo_operacion", None)
+        valor_1 = data.get("valor_1", None)
+        valor_2 = data.get("valor_2", None)
+        print(data)
+        print(tipo, valor_1, valor_2)
         resultado = 0
         errores = []
         if not tipo:
-            errores[{"tipo":"Especifique el tipo de la operación."}]
-        if not valor_1:
-            errores[{"valor_1":"Ingrese el primer valor."}]
-        if not valor_2:
-            errores[{"valor_2":"Ingrese el segundo valor."}]
-        if type(valor_1) != int or type(valor_2) != int:
-            errores[{"error_general":"Los valores deben ser números enteros."}]
+            errores.append({"tipo":"Especifique el tipo de la operación."})
+        if not valor_1 and tipo != "raiz_cuadrada":
+            errores.append({"valor_1":"Ingrese el primer valor."})
+        if not valor_2 and tipo != "porcentaje":
+            errores.append({"valor_2":"Ingrese el segundo valor."})
+        
         
         else:
             if tipo == "suma":
@@ -29,13 +35,18 @@ class CalculadoraView(View):
                 resultado = int(valor_1) * int(valor_2)
             elif tipo == "division":
                 if valor_2 == 0:
-                    errores[{"valor_2":"No se puede dividir por cero."}]
+                    errores.append({"valor_2":"No se puede dividir por cero."})
                 resultado = int(valor_1) / int(valor_2)
             elif tipo == "potencia":
                 resultado = int(valor_1) ** int(valor_2)
-            elif tipo == "modulo":
-                resultado = int(valor_1) % int(valor_2)
+            elif tipo == "porcentaje":
+                resultado = int(valor_1) * 0.01
             elif tipo == "raiz_cuadrada":
-                resultado = int(valor_1) ** (1/int(valor_2))
-        return render(request, "index.html", {"resultado": resultado, "errores": errores})
+                resultado = math.sqrt(int(valor_2))
+        # Si hay errores, devolverlos
+        if errores:
+            return JsonResponse(errores, status=400, safe=False)
+
+        # Si no hay errores, devolver el resultado
+        return JsonResponse({"resultado": resultado}, status=200)
 
